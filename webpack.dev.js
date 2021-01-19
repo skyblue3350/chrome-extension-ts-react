8064371b511c7a0061dfbbf86a58003b90616943
sky <skyblue3350@gmail.com>
2021-01-20 01:41:01 +0900

    add extention reloader

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,19 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const merge = require('webpack-merge')
+const ExtensionReloader = require('webpack-extension-reloader')
+const common = require('./webpack.common.js')
+const entries = require('./webpack.entries')
 
 module.exports = merge(common, {
     mode: 'development',
     watch: true,
+    plugins: [
+        new ExtensionReloader({
+            entries: {
+                background: 'background',
+                options: 'option',
+                popup: 'popup',
+                contentScript: Object.keys(entries.contentScripts),
+            },
+        }),
+    ]
 });
